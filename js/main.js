50a4888c9d97ec8ad4a5da64154b59524fb32ae6
Max Herzig <work@maxherzig.com>
2017-08-02 23:24:41 -0700

    added new clients, certs

--- js/main.js
+++ js/main.js
@@ -4,18 +4,16 @@
   'use strict';
 
   if ('querySelectorAll' in document && 'classList' in document.createElement('div')) {
-    (function () {
-      var subs = document.querySelectorAll('h2 span'),
-          curIndex = 0;
+    var subs = document.querySelectorAll('h2 span'),
+        curIndex = 0;
 
-      setInterval(function () {
-        document.querySelector('h2 span.placed').classList.remove('placed');
-        curIndex++;
-        if (curIndex >= subs.length) {
-          curIndex = 0;
-        }
-        subs[curIndex].classList.add('placed');
-      }, 2500);
-    })();
+    setInterval(function () {
+      document.querySelector('h2 span.placed').classList.remove('placed');
+      curIndex++;
+      if (curIndex >= subs.length) {
+        curIndex = 0;
+      }
+      subs[curIndex].classList.add('placed');
+    }, 2500);
   }
 })();
